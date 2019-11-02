92a5a7d4a2100fee42fcc7068239a437e477d373
Herman Liu <hermanliu95@gmail.com>
2019-11-01 17:19:34 -0700

    added a table for the train section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,28 @@
-$(document).ready(function(){
-    console.log('hello world');
+$(document).ready(function () {
+    let firebaseConfig = {
+        apiKey: "AIzaSyASWqpxfGdsi7mQCq-7zIfL66lXocyZsZk",
+        authDomain: "train-time-database88.firebaseapp.com",
+        databaseURL: "https://train-time-database88.firebaseio.com",
+        projectId: "train-time-database88",
+        storageBucket: "train-time-database88.appspot.com",
+        messagingSenderId: "224255532992",
+        appId: "1:224255532992:web:9af7d286016333c33a3225"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    const database = firebase.database();
+    console.log(database);
     
+
+
+
+
+
+
+
+
+
+
+
 })
