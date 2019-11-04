bb1a044f3ad7607e6420d78dc8fadd429e7067f0
Herman Liu <hermanliu95@gmail.com>
2019-11-03 16:17:26 -0800

    created click function that takes data from form and stores in firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,21 +12,16 @@ $(document).ready(function () {
     firebase.initializeApp(firebaseConfig);
 
     const database = firebase.database();
-    //console.log(database);
-    
-//Create a button for adding new trains
-//Create a way to retreive data from firebase and implement it to the train database
-//Create a way to show the current times and train arrivals
 
-//create a function that takes the values from the forms and input it to the trains section
+//Click function takes data from the form and uploads it to firebase 
 $('#submit').on('click', function(e){
     e.preventDefault(); 
 
     //grabs the user input from the form
     var trainName = $('#trainName').val().trim();
     var destination = $('#destination').val().trim();
-    var firstTrainTime = $('#firstTrainTime').val().trim();
-    var frequencyMin = $('#frequencyMin').val().trim();
+    var firstTrainTime = $('#firstTrainTime').val().trim(); //Need to work on this line of code,
+    var frequencyMin = $('#frequencyMin').val().trim(); //Need to work on this line of code,
 
     //Create a local 'temporary' object for holding train data
     var newTrain = {
@@ -47,8 +42,12 @@ $('#submit').on('click', function(e){
 
     alert('Train Successfully Added');
     
-    
-})
+    //Clear all the input from the form 
+    $('#trainName').val("")
+    $('#destination').val("")
+    $('#firstTrainTime').val("")
+    $('#frequencyMin').val("")
+});
 
 
 
