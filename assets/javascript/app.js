53b2d88e848ed5bf45d8ff89924298562fc22891
Herman Liu <hermanliu95@gmail.com>
2019-11-03 16:06:36 -0800

    successfully able to inpiut data from train input to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,17 +12,43 @@ $(document).ready(function () {
     firebase.initializeApp(firebaseConfig);
 
     const database = firebase.database();
-    console.log(database);
+    //console.log(database);
     
+//Create a button for adding new trains
+//Create a way to retreive data from firebase and implement it to the train database
+//Create a way to show the current times and train arrivals
 
 //create a function that takes the values from the forms and input it to the trains section
+$('#submit').on('click', function(e){
+    e.preventDefault(); 
 
-$('#submit').on('click', function () {
-    let trainName = $('#trainName').val();
-    trainName.append('#newTrain');
+    //grabs the user input from the form
+    var trainName = $('#trainName').val().trim();
+    var destination = $('#destination').val().trim();
+    var firstTrainTime = $('#firstTrainTime').val().trim();
+    var frequencyMin = $('#frequencyMin').val().trim();
+
+    //Create a local 'temporary' object for holding train data
+    var newTrain = {
+        name: trainName,
+        destination: destination,
+        time: firstTrainTime,
+        frequency: frequencyMin
+    };
+
+    //Uploads this data to firebase database
+    database.ref().push(newTrain);
+
+    //Log everything in the console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrainTime);
+    console.log(newTrain.frequencyMin);
+
+    alert('Train Successfully Added');
+    
     
 })
-
 
 
 
